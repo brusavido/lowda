2952246f9357a2960d9997dfd13690d48cf33e75
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-25 01:53:05 +0800

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/brusavido/lowda/raw/main/lowda
-wget https://raw.githubusercontent.com/brusavido/lol/main/gpu.sh
+wget https://raw.githubusercontent.com/brusavido/lowda/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
